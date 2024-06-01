8d59f0575fb7c3daa5f636d8a7e08a973cfdcde0
doremihyeon <algus04617@gmail.com>
2024-06-01 23:57:42 +0900

    proceeding

---
+++ STEP_10/H1/H1_GUI_1.java
@@ -0,0 +1,14 @@
+package STEP_10.H1
+
+import java.awt.*;
+import javax.swing.*;
+
+public class H1_GUI_1 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GUI_1");
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLUE);
+        
+    }
+}
